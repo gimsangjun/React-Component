4e31eecb1a4f0ad08d8a7ac974a21031dd5003a6
gimsangjun <5321913@naver.com>
2024-04-01 15:41:33 +0900

    refactor: twin.macro 적용

--- src/pages/RootPage.js
+++ src/pages/RootPage.js
@@ -1,32 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import tw from "twin.macro";
 
 export default function Root() {
   return (
     <div className="flex justify-center items-center">
-      {/* TODO: header component 만들기 */}
-      <nav className="flex space-x-4">
-        <li>
+      <ul className="grid grid-cols-1 gap-4">
+        <List>
           <Link className="text-blue-500 hover:text-blue-700" to="/">
             Home
           </Link>
-        </li>
-        <li>
+        </List>
+        <List>
           <Link className="text-blue-500 hover:text-blue-700" to="/todos">
             Redux 사용 예제 : Todos
           </Link>
-        </li>
-        <li>
+        </List>
+        <List>
           <Link className="text-blue-500 hover:text-blue-700" to="/counter">
             Redux + 미들웨어 비동기 setTime: Counter
           </Link>
-        </li>
-        <li>
+        </List>
+        <List>
           <Link className="text-blue-500 hover:text-blue-700" to="/post">
             Redux + 미들웨어 api : Pages
           </Link>
-        </li>
-      </nav>
+        </List>
+      </ul>
     </div>
   );
 }
+
+const List = tw.li`p-4 bg-gray-100 rounded-md shadow-md`;
