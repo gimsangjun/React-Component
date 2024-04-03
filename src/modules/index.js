bf9b97e3ec98b5941ea24e646ab1e9d1c617ddf6
gimsangjun <5321913@naver.com>
2024-04-03 16:46:24 +0900

    feat: add noticeboard modules

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import todos from "./todos";
 import counter from "./counter";
 import posts from "./posts";
+import noticeboard from "./noticeboard";
 
 // 여러개의 리듀서가 있을경우 이렇게 루트 리듀서로 하나로 묶을수 있다.
 const rootReducer = combineReducers({
   todos,
   counter,
   posts,
+  noticeboard,
 });
 
 export default rootReducer;
